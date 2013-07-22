d6384bb26f243ba2f223a4ff78f00e97bd1325e0
vagrant <vagrant@precise64.(none)>
2013-07-22 07:46:04 +0000

    started in on the thinkster.io angular tutorials. Nothing in the way of progress towards a stock ticker. Will need to tear it apart once I have the basics of angular down.

--- app/js/app.js
+++ app/js/app.js
@@ -1,3 +1,17 @@
-(function(){
-	console.log("Up and running!");
-})();
+var myApp = angular.module('myApp', []);
+
+myApp.factory('Data', function(){
+	return {message: "Im' data from a service"};
+})
+
+myApp.controller('FirstCtrl', function ($scope, Data){
+	$scope.data = Data;
+});
+
+myApp.controller('SecondCtrl', function ($scope, Data) {
+	$scope.data = Data;
+
+	$scope.reverseMessage = function(message) {
+		return message.split("").reverse().join("");
+	}
+});
